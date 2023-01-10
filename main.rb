5d088fa3021dd0d3d0c21375ec022f5fa07fd805
John P <70922864+jpierce-coder@users.noreply.github.com>
2023-01-10 00:17:53 -0600

    Initial commit

---
+++ main.rb
@@ -0,0 +1,60 @@
+####### Below is an example of a hash
+
+
+
+# me = {
+
+#   name: "John",
+
+#   age: 29
+
+# }
+
+
+
+# puts me[:age]
+
+
+
+####### Below is an array
+
+
+
+# me = [
+
+#   "John",
+
+#   29
+
+# ]
+
+
+
+# puts me[1]
+
+# puts me[0]
+
+
+
+## PUTS
+
+# puts "Hello World"
+
+
+
+# ## P
+
+
+
+# p "Hello World"
+
+
+
+API_KEY = 'abc123'
+
+message = "Hi there, fellow coders! Here is the #{API_KEY}!"
+
+
+
+puts message
+
